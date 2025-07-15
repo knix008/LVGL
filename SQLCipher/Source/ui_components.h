024c527e77d68568d89b704073ec7126147c3999
Suho Kwon <suho.kwon@gmail.com>
2025-07-15 13:38:19 +0900

    Add SQLCipher Example.

---
+++ SQLCipher/Source/ui_components.h
@@ -0,0 +1,25 @@
+#ifndef UI_COMPONENTS_H
+#define UI_COMPONENTS_H
+
+#include "lvgl.h"
+
+// UI initialization and setup
+void lv_example_simple_gui(void);
+void lv_example_tab_menu(void);
+
+// Button event callbacks
+void btn_event_cb(lv_event_t * e);
+void btn2_event_cb(lv_event_t * e);
+
+// Tab event callbacks
+void tab_event_cb(lv_event_t * e);
+void slider_event_cb(lv_event_t * e);
+void switch_event_cb(lv_event_t * e);
+
+// Number keypad callbacks
+void keypad_number_cb(lv_event_t * e);
+void keypad_clear_cb(lv_event_t * e);
+void keypad_enter_cb(lv_event_t * e);
+void keypad_backspace_cb(lv_event_t * e);
+
+#endif // UI_COMPONENTS_H 
