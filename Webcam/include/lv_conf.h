ae1a0107429d61ca2fc59b9fc4f3d7a62b708bf8
Suho Kwon <suho.kwon@gmail.com>
2025-08-11 09:08:57 +0900

    Add chagnes.

--- Webcam/include/lv_conf.h
+++ Webcam/include/lv_conf.h
@@ -1,3 +1,4 @@
 // See LVGL documentation for configuration options
 // This file is required for LVGL build. You can customize as needed.
-#include "lvgl/lv_conf_template.h"
+#include "../lvgl/lv_conf_template.h"
+
