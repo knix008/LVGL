3f3329311c4eccd3afaa9e462655e7455877294e
Suho Kwon <suho.kwon@gmail.com>
2025-09-01 13:43:49 +0900

    add chagnes.

--- FaceRecognition2.0-MariaDB/Source/src/main.c
+++ FaceRecognition2.0-MariaDB/Source/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <SDL2/SDL.h>
 #include "lvgl.h"
 #include "lv_drivers.h"
 #include "ui_components.h"
@@ -7,6 +8,25 @@
 #include "lv_freetype.h"
 #include "lv_ffmpeg.h"
 #include "web_server.h"
+
+// Global quit flag
+static volatile bool quit_requested = false;
+
+// Custom keyboard event handler for quit key
+void handle_quit_key(SDL_Event *event) {
+    if (event->type == SDL_KEYDOWN) {
+        // Check for Q key to quit
+        if (event->key.keysym.sym == SDLK_q) {
+            printf("\nQuit key (Q) pressed. Exiting...\n");
+            quit_requested = true;
+        }
+        // Also support Escape key
+        else if (event->key.keysym.sym == SDLK_ESCAPE) {
+            printf("\nEscape key pressed. Exiting...\n");
+            quit_requested = true;
+        }
+    }
+}
 
 // In your main loop, call lv_timer_handler() periodically, e.g., every 5-10 ms
 int main(void)
@@ -52,13 +72,20 @@ int main(void)
 
     printf("Tab menu GUI created successfully. Window should appear now.\n");
     printf("Web interface available at: http://localhost:8080\n");
-    printf("Press Ctrl+C to exit.\n");
+    printf("Press Q or Escape to exit.\n");
 
-    while(1) {
+    while(!quit_requested) {
+        // Handle SDL events for quit keys
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            handle_quit_key(&event);
+        }
+        
         lv_timer_handler();
         web_server_poll();  // Poll web server
         usleep(5000);
     }
 
+    printf("Shutting down...\n");
     return 0;
 }
